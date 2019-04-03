4eec848a1b4ef22ea0c0ab9ff466e4af36226322
TheCoolRob <robert.bennetto.za@gmail.com>
2019-04-03 23:31:14 +0200

    playing around with the boost compile. Seems straight forward enough

--- DAGs/graphPlus.R
+++ DAGs/graphPlus.R
@@ -1,17 +1,21 @@
 
 
 # ref class for edge
-state <- setRefClass("vertex", fields = list(id = "numeric", description = "character"), 
+state <- setRefClass("vertex", fields = list(id = "numeric", 
+                                             description = "character"), 
                      methods = list(
                        show = function(){
-                          cat("State\nDescription:",description, "\nID:",id,"\n")
+                          cat("State\nDescription:", description, "\nID:",id,"\n")
                        }
                      ))
 
-transition <- setRefClass("edge", fields = list(from = "vertex", to = "vertex", weight = "numeric", description ="character"),
+transition <- setRefClass("edge", fields = list(from = "vertex", 
+                                                to = "vertex", 
+                                                weight = "numeric", 
+                                                description ="character"),
                           methods = list(
                             show = function(){
-                              cat("Edge\nDescription:",description,"\nWeight:",weight, "\nFrom:\n")
+                              cat("Edge\nDescription:",description,"\nWeight:", weight, "\nFrom:\n")
                               from$show()
                               cat("To:\n")
                               to$show()
@@ -19,26 +23,29 @@ transition <- setRefClass("edge", fields = list(from = "vertex", to = "vertex", 
 )
 
 adjList <- setRefClass("adjList", fields = list(adjs = "list"),
-                       methods = list(
-                         addEdge = function(e){
-                           if(!("edge" %in% class(e))){
-                             stop("Cannot add non edge class to adjacency list")
+                         methods = list(
+                           addEdge = function(e){
+                             if(!("edge" %in% class(e))){
+                               stop("Cannot add non edge class to adjacency list")
+                             }
+                             vName <- paste0("v",e$from$id)
+                             adjs[[vName]] <<- c(adjs[[vName]],e$to$id)
+                             cat("Edge Added")
+                           },
+                           show = function(){
+                             vNames <- names(adjs)
+                             for(vN in vNames){
+                              cat("Vertex:",vN,"\n")
+                              cat("Adjs:",adjs[[vN]],"\n")
+                             }
                            }
-                           vName <- paste0("v",e$from$id)
-                           adjs[[vName]] <<- c(adjs[[vName]],e$to$id)
-                           cat("Edge Added")
-                         },
-                         show = function(){
-                           vNames <- names(adjs)
-                           for(vN in vNames){
-                            cat("Vertex:",vN,"\n")
-                            cat("Adjs:",adjs[[vN]],"\n")
-                           }
-                         }
-                       )
+                         )
                        )
 
-dag <- setRefClass("dag", fields = list( vertices = "list", edges = "list", adjList = "adjList", vertexCount = "numeric"),
+dag <- setRefClass("dag", fields = list(vertices = "list",
+                                        edges = "list",
+                                        adjList = "adjList",
+                                        vertexCount = "numeric"),
                   methods = list(
                     newVertex = function(description){
                       v <- state(id = vertexCount, description = description )
@@ -47,16 +54,20 @@ dag <- setRefClass("dag", fields = list( vertices = "list", edges = "list", adjL
                       return(v)
                     },
                     createTransition = function(from, to, weight, description){
-                      
                       e <- transition(from = from, to = to, weight= weight, description = description)
                       edges <<- c(edges,e)
                       adjList$addEdge(e)
                       
                       return(e)
+                    },
+                    testMe = function(){
+                      boost_sample()
                     }
                   ))
 
 g <- dag(vertexCount = 0)
+
+
 
 awake <- g$newVertex(description = "Awake")
 awake
@@ -73,7 +84,38 @@ getDressedA
 getDressedB <- g$createTransition(from = awake,to = dressedB, weight = 0.5, description = "Getting cool")
 getDressedB
 
-g$adjList
 
+
+# 
+# setClass("GTest", slots = c(graph = "numeric"))
+# setGeneric("graph", function(x) standardGeneric("graph"))
+# setMethod("graph", "GTest", function(x) x@graph)
+# 
+# g <- new("GTest")
+# g
+# 
+# graph()
+# 
+# G <- setRefClass("g_test", 
+#                  fields = list(vertices = "list", 
+#                                edges = "list",
+#                                adjList = "adjList",
+#                                vertexCount = "numeric"),
+#                    methods = list(
+#                      newVertex = function(description){
+#                        v <- state(id = vertexCount, description = description )
+#                        vertexCount <<- vertexCount + 1
+#                        vertices <<- c(vertices,v)
+#                        return(v)
+#                      },
+#                      createTransition = function(from, to, weight, description){
+#                        
+#                        e <- transition(from = from, to = to, weight= weight, description = description)
+#                        edges <<- c(edges,e)
+#                        adjList$addEdge(e)
+#                        
+#                        return(e)
+#                      }
+#                    ))
 
 
---
+++ DAGs/graph_fns.cpp
@@ -0,0 +1,102 @@
+// assumes you have the boost libraries installed. 
+// see https://www.boost.org/ for the source. 
+
+// [[Rcpp::plugins(cpp14)]]                                        
+// note: this line above enables the c14 compiler which is required for the specific syntax of that version of cpp
+#include <Rcpp.h>
+#include <iostream>
+#include <deque>
+#include <iterator>
+
+#include "boost/graph/adjacency_list.hpp"
+#include "boost/graph/topological_sort.hpp"
+#include "boost/graph/dijkstra_shortest_paths.hpp"
+using namespace Rcpp;
+
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+
+
+typedef boost::property<boost::edge_weight_t, int> EdgeWeightProperty;
+typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, boost::no_property, EdgeWeightProperty > DirectedGraph;
+typedef boost::graph_traits<DirectedGraph>::edge_iterator edge_iterator;
+
+
+// [[Rcpp::export]]
+NumericVector boost_sample_2(){
+  DirectedGraph* g = new DirectedGraph();
+  boost::add_edge (0, 1, 8, *g);
+  boost::add_edge (0, 3, 18, *g);
+  std::pair<edge_iterator, edge_iterator> ei = edges(*g);
+  std::cout << "Number of edges = " << num_edges(*g) << "\n";
+  std::cout << "Edge list:\n";
+  
+  std::copy( ei.first, ei.second,
+             std::ostream_iterator<boost::adjacency_list<>::edge_descriptor>{
+               std::cout, "\n"});
+  
+  std::cout << std::endl;
+  return 0;
+}
+
+// [[Rcpp::export]]
+NumericVector shortest_path_a_b(int a, int b, NumericVector froms, NumericVector tos, NumericVector weights){
+  DirectedGraph g;
+  
+  dijkstra_shortest_paths(g, s, predecessor_map(&p[0]).distance_map(&d[0]));
+  
+}
+
+// [[Rcpp::export]]
+NumericVector boost_sample(){
+  DirectedGraph g;
+  
+  boost::add_edge (0, 1, 8, g);
+  boost::add_edge (0, 3, 18, g);
+  boost::add_edge (1, 2, 20, g);
+  boost::add_edge (2, 3, 2, g);
+  boost::add_edge (3, 1, 1, g);
+  boost::add_edge (1, 3, 7, g);
+  boost::add_edge (1, 4, 1, g);
+  boost::add_edge (4, 5, 6, g);
+  boost::add_edge (2, 5, 7, g);
+  
+  std::pair<edge_iterator, edge_iterator> ei = edges(g);
+  
+  std::cout << "Number of edges = " << num_edges(g) << "\n";
+  std::cout << "Edge list:\n";
+
+  std::copy( ei.first, ei.second,
+             std::ostream_iterator<boost::adjacency_list<>::edge_descriptor>{
+               std::cout, "\n"});
+
+  std::cout << std::endl;
+  
+  return 0;
+}
+
+// [[Rcpp::export]]
+NumericVector timesTwo(NumericVector x) {
+  return x * 2;
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+timesTwo(42)
+boost_sample()
+boost_sample_2();
+*/
